0289811149cbb562d57d2220b8cd85d36aa718e4
ketilyn santos <santosketilyn1510@gmail.com>
2026-06-18 12:05:34 -0300

    Add files via upload
    adição dos produtos

--- loja.sql
+++ loja.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 21-Maio-2026 às 17:26
--- Versão do servidor: 10.4.22-MariaDB
--- versão do PHP: 8.1.2
+-- Tempo de geração: 18/06/2026 às 00:27
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,69 +26,98 @@ USE `loja`;
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `categorias`
+-- Estrutura para tabela `categorias`
 --
 
 CREATE TABLE `categorias` (
   `id_categoria` int(11) NOT NULL,
   `categoria` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `categorias`
+--
+
+INSERT INTO `categorias` (`id_categoria`, `categoria`) VALUES
+(1, 'eletronicos'),
+(2, 'alimentos'),
+(3, 'roupas'),
+(4, 'limpeza'),
+(5, 'higiene');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `produtos`
+-- Estrutura para tabela `produtos`
 --
 
 CREATE TABLE `produtos` (
   `id_produto` int(11) NOT NULL,
   `nome` varchar(255) NOT NULL,
   `preco` decimal(10,2) NOT NULL,
-  `quantidade` int(11) NOT NULL,
-  `marca` varchar(255) DEFAULT NULL,
+  `quant` int(11) NOT NULL,
   `observacoes` varchar(255) DEFAULT NULL,
-  `id_categoria` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id_categoria` int(11) DEFAULT NULL,
+  `data_cadastro` date DEFAULT NULL,
+  `marca` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id_produto`, `nome`, `preco`, `quant`, `observacoes`, `id_categoria`, `data_cadastro`, `marca`) VALUES
+(1, 'papel higienico', 17.50, 20, '', 5, '2026-06-11', '0000-00-00'),
+(2, 'macarrao', 4.90, 150, '', 2, '2026-06-11', '0000-00-00'),
+(3, 'vestidos', 49.90, 50, 'Tamanhos p/m/g', 3, '2026-06-11', '0000-00-00'),
+(4, 'desinfetante', 6.40, 200, '', 4, '2026-06-11', '0000-00-00'),
+(5, 'celular', 1.50, 40, '', 1, '2026-06-11', '0000-00-00'),
+(6, 'sabonete', 1.50, 140, '', 5, '2026-06-11', '0000-00-00'),
+(7, 'detergente', 2.50, 240, '', 4, '2026-06-11', '0000-00-00'),
+(8, 'fone', 50.00, 30, '', 1, '2026-06-11', '0000-00-00'),
+(9, 'moletom', 80.00, 60, 'Tamanhos p/m/g', 3, '2026-06-11', '0000-00-00'),
+(10, 'arroz', 25.00, 160, '', 2, '2026-06-11', '0000-00-00'),
+(11, 'feijao', 9.90, 160, '', 2, '2026-06-11', '0000-00-00');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices para tabela `categorias`
+-- Índices de tabela `categorias`
 --
 ALTER TABLE `categorias`
   ADD PRIMARY KEY (`id_categoria`);
 
 --
--- Índices para tabela `produtos`
+-- Índices de tabela `produtos`
 --
 ALTER TABLE `produtos`
   ADD PRIMARY KEY (`id_produto`),
   ADD KEY `id_categoria` (`id_categoria`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- Restrições para despejos de tabelas
+-- Restrições para tabelas despejadas
 --
 
 --
--- Limitadores para a tabela `produtos`
+-- Restrições para tabelas `produtos`
 --
 ALTER TABLE `produtos`
   ADD CONSTRAINT `produtos_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id_categoria`);
